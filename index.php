88fb748ca1b670610e2f7444b3798925bf560240
root <root@rafaelvelez.us>
2014-08-29 13:20:50 +0000

    Fixed re-direct

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
-$loc='location: http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}/modern}"
-
+$loc='location: http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}/modern";
+echo $loc;
 header( $loc ) ;
-
-
 
 ?>
 
